8673766a7055734a96556ef4bb386ce17e9c1866
sbwfnyiiiu <sbwfnyiiiu@163.com>
2018-01-26 08:52:21 +0800

    Clock APP

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-let sitename = 'CAICENT'
-let obj = {
-  fname : 'Lee',
-  lname : 'ChuanMiao',
-}
-const element = <h1>Welcome to { sitename }</h1>
-
-function cartoon(name,show){
-  return <div>
-    <h1 className="hello">Welcom to {name}</h1>
-    <h2>Show time is {show}</h2>
-  </div>
-}
-ReactDOM.render(
-  cartoon('xxx','2018'),
-    document.getElementById('root')
-);
+setInterval(function(){
+  ReactDOM.render(
+    <h1>{new Date().toLocaleTimeString()}</h1>,
+      document.getElementById('root')
+  );
+},1000);
